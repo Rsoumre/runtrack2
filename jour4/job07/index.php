e0026a37b855f6ef96474de0cb17ea26a2a81974
Rsoumre <harounasoumare547@gmail.com>
2025-07-25 14:56:12 +0200

    Job07

--- jour4/job07/index.php
+++ jour4/job07/index.php
@@ -6,6 +6,7 @@
     <input type="submit" value="Construire la maison">
 </form>
 
+<pre>
 <?php
 if (isset($_GET["largeur"]) && isset($_GET["hauteur"])) {
     $largeur = $_GET["largeur"];
@@ -15,22 +16,39 @@ if (isset($_GET["largeur"]) && isset($_GET["hauteur"])) {
         $largeur = intval($largeur);
         $hauteur = intval($hauteur);
 
-        // Toit
-        for ($i = 0; $i < $largeur; $i++) {
-            echo "^";
+        if ($largeur % 2 == 0) {
+            $largeur += 1; // Pour garder une symétrie dans le toit
         }
-        echo "<br>";
 
-        // Corps
+        // ----- TOIT -----
+        $etoiles = 1;
+        $espaces = floor($largeur / 2);
+
+        for ($i = 0; $i < floor($largeur / 2) + 1; $i++) {
+            echo str_repeat(" ", $espaces);
+            echo "/";
+            echo str_repeat("*", $etoiles);
+            echo "\\";
+            echo "\n";
+
+            $espaces -= 1;
+            $etoiles += 2;
+        }
+
+        // ----- CORPS -----
         for ($j = 0; $j < $hauteur; $j++) {
             echo "|";
-            for ($i = 0; $i < $largeur - 2; $i++) {
-                echo "_";
-            }
-            echo "|<br>";
+            echo str_repeat(" ", $largeur);
+            echo "|\n";
         }
+
+        // ----- BASE -----
+        echo "|";
+        echo str_repeat("_", $largeur);
+        echo "|\n";
     } else {
         echo "Veuillez entrer des nombres valides.";
     }
 }
 ?>
+</pre>
